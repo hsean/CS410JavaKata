e2398433565b5a02fd65c34bb3f0b46d24a87aa6
Shane McBroom <mcbroom@pdx.edu>
2021-07-07 19:12:55 -0700

    1.0

--- FooBarQix/src/main/java/edu/pdx/cs410J/hsean/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/hsean/Kata.java
@@ -1,4 +1,4 @@
-package edu.pdx.cs410J.hsean;
+package edu.pdx.cs410J.mcbroom;
 
 /**
  * A class for getting started with a code kata
@@ -6,11 +6,62 @@ package edu.pdx.cs410J.hsean;
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
+
 public class Kata {
-                                                                                    
+
+  public static void FooBarQix(String s) {
+    int num = Integer.parseInt(s);
+    boolean foobarqix = false;
+
+    System.out.print(s + " => ");
+
+    if(num % 3 == 0) {
+      foobarqix = true;
+      System.out.print("Foo");
+
+      for(int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '3') {
+          System.out.print("Foo");
+        }
+      }
+    }
+
+    if(num % 5 == 0) {
+      foobarqix = true;
+      System.out.print("Bar");
+
+      for(int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '5') {
+          System.out.print("Bar");
+        }
+      }
+    }
+
+    if(num % 7 == 0) {
+      foobarqix = true;
+      System.out.print("Qix");
+
+      for(int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '7') {
+          System.out.print("Qix");
+        }
+      }
+    }
+
+    if(!foobarqix) {
+      System.out.println(s);
+    }
+  }
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
+
+    if(args.length < 1) {
+      System.err.println("Missing command line arguments");
+      System.exit(1);
+    }
+
+    FooBarQix(args[0]);
+    System.exit(0);
   }
 }
+
